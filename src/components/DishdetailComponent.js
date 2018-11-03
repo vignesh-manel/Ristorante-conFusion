73b933f4497bd009a19cfca76a1afb86339a2bf3
vmanel <vnayak83@gmail.com>
2018-11-03 10:13:01 +0000

    Presentational and Container Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react';
 import { Card,CardImg,CardText,CardBody,CardTitle } from 'reactstrap';
 
 class DishDetail extends Component {
-    constructor(props) {
-	super(props);
-    }
-
 
     renderDish(dish) {
 	if (dish != null) {
@@ -33,13 +29,13 @@ class DishDetail extends Component {
 	var list = [];
 	for(var i=0; i<4; i++)
 	{
-	    list.push(<li class="mb-2">{comments[i].comment}</li>);
-	    list.push(<li class="mb-2"><span>--{comments[i].author}</span>,<span>{new Date(comments[i].date).toLocaleDateString("en-US",options)}</span></li>);
+	    list.push(<li className="mb-2">{comments[i].comment}</li>);
+	    list.push(<li className="mb-2"><span>--{comments[i].author}</span>,<span>{new Date(comments[i].date).toLocaleDateString("en-US",options)}</span></li>);
 	}
 	return (
 	    <div>
 	    <h3>Comments</h3>
-	    <ul class="list-unstyled">
+	    <ul className="list-unstyled">
 		{list}
 	    </ul>
 	    </div>
@@ -53,15 +49,17 @@ class DishDetail extends Component {
     }
 
     render() {
-	const dish = this.props.selectedDish;
+	const dish = this.props.dish;
 	return (
-	    <div class="row">
-		<div class="col-12 col-md-5 m-1">
+	   <div className="container">
+	    <div className="row">
+		<div className="col-12 col-md-5 m-1">
 		    {this.renderDish(dish)}
 		</div>
-		<div class="col-12 col-md-5 m-1">
+		<div className="col-12 col-md-5 m-1">
 		    {this.renderComments(dish)}
 		</div>
+	    </div>
 	    </div>
     );
     }
